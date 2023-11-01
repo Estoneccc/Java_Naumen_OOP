61d1ed8b48a37bd7863882727232b7067e7e05b8
Estoneccc <kutyavin.danil@mail.ru>
2023-11-01 18:10:34 +0500

    Сделал задание 1

---
+++ src/main/java/ru/oop/Car.java
@@ -0,0 +1,14 @@
+package ru.oop;
+
+/**
+ * Машина
+ */
+public class Car {
+    public Car(Person person) {
+    }
+    /**
+     * Проехать на машине до назначенной точки
+     */
+    public void drive(Position position) {
+    }
+}
